57b5dc11f661fcb8aa1bbf1c9bbb6528d8c80abe
Cindy <cdyy2400@gmail.com>
2022-12-16 20:29:39 +0700

    edit header

--- components/ui/Header.js
+++ components/ui/Header.js
@@ -5,17 +5,17 @@ const Header = () => {
     <div class="bg-white dark:bg-slate-900 px-6 py-4 ring-1 ring-slate-900/5 shadow-md mb-4">
       <div class="flex flex-row ">
         <div class="grow">
-          <h2 class="text-base font-xl tracking-tight font-medium text-xl">
+          <h2 class="text-base font-xl tracking-tight font-large text-2xl">
             Todo App
           </h2>
         </div>
         <div class="basis-1/12 ">
-          <Link href="/" className="underline decoration-indigo-500">
+          <Link href="/" className="font-semibold">
             Home
           </Link>
         </div>
         <div class="basis-1/12 ">
-          <Link href="/new-todo" className="underline decoration-indigo-500">
+          <Link href="/new-todo" className="font-semibold">
             Add Todo
           </Link>
         </div>
